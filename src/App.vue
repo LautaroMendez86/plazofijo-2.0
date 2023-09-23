66b698d5f5f3756d1bd1f7888c6bc19529d1f093
LautaroMendez86 <lautaromendez123@gmail.com>
2023-09-23 17:54:46 -0300

    feat: create project and add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
+<script setup>
+import MainNavbar from "@/components/navbar/MainNavbar.vue";
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
+  <main-navbar />
 </template>
 
 <style>
---
+++ src/components/navbar/NavItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-link :to="props.route" class="text-gray-400 font-bold">
+    <div class="flex flex-col items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" :d="props.icon" />
+      </svg>
+      <span>{{ props.label }}</span>
+    </div>
+  </router-link>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  label: String,
+  icon: String,
+  route: String,
+});
+</script>
